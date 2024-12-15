83692e75641d21b6a7fe1f70dc5a9f90a3f54f45
On0n0k1 <lucas.lemos.kaihatsusha@gmail.com>
2024-12-15 10:42:09 -0300

    feat: retrieving unique topics and retrieving all projects
    Signed-off-by: On0n0k1 <lucas.lemos.kaihatsusha@gmail.com>

--- src/project_sqlite.rs
+++ src/project_sqlite.rs
@@ -1,10 +1,13 @@
 use std::collections::{HashMap, HashSet};
+use std::sync::mpsc::Iter;
 
 use clap::builder::Str;
 use serde::{Deserialize, Serialize};
 use sqlite::Connection;
 use sqlite::Error;
 use sqlite::Value;
+
+use crate::projects::SearchParameter;
 
 #[derive(Debug, Default, Deserialize)]
 pub struct Projects {
@@ -19,9 +22,9 @@ pub struct Data {
 impl Data {
     pub fn new(projects: Vec<Project>) -> Self {
         let mut topics: HashSet<String> = HashSet::new();
-        println!("Starting connection...");
+        // println!("Starting connection...");
         let connection: Connection = sqlite::open(":memory:").unwrap();
-        println!("Checking projects...");
+        // println!("Checking projects...");
         for project in projects.iter() {
             for key in project.topics.keys() {
                 if !topics.contains(key) {
@@ -30,7 +33,7 @@ impl Data {
             }
         }
         let mut topics: Vec<String> = topics.into_iter().collect::<Vec<String>>();
-        println!("Found these unique keys: {topics:?}\n");
+        // println!("Found these unique keys: {topics:?}\n");
         let mut keys = topics
             .iter()
             .to_owned()
@@ -41,9 +44,9 @@ impl Data {
             keys = format!(", {keys}");
         }
         let query = format!("CREATE TABLE projects (source TEXT PRIMARY KEY{keys});");
-        println!("Running Query:\n{query}\n");
+        // println!("Running Query:\n{query}\n");
         connection.execute(query).unwrap();
-        println!("Query executed successfully");
+        // println!("Query executed successfully");
 
         let mut keys = topics.to_vec().join(", ");
         if !keys.is_empty() {
@@ -63,47 +66,143 @@ impl Data {
             }
             let query = format!("INSERT INTO projects ({keys}) VALUES ({values});");
 
-            println!("Running: \n{query}\n");
+            // println!("Running: \n{query}\n");
             connection.execute(query).unwrap();
         }
 
-        let query = "SELECT * FROM projects;";
-        topics.insert(0, "source".to_string());
-        for row in connection
+        // let query = "SELECT * FROM projects;";
+        // topics.insert(0, "source".to_string());
+        // for row in connection
+        //     .prepare(query)
+        //     .unwrap()
+        //     .into_iter()
+        //     .map(|row| row.unwrap())
+        // {
+        //     for (index, topic) in topics.iter().enumerate() {
+        //         if row.contains(index) {
+        //             let value: Result<&str, Error> = row.try_read::<&str, _>(index);
+        //             if let Ok(value) = value {
+        //                 println!("{topic}: {}", value);
+        //             }
+        //         }
+        //     }
+        //     println!("\n-------------------------------------\n");
+        // }
+        let output = Self { connection, topics };
+        // println!("Retrieving topics for deployment: ");
+        // let result = output.retrieve_topics(Default::default(), "deployment");
+        // println!("Output: {result:?}");
+        // println!("Retrieving topics for platform: ");
+        // let result = output.retrieve_topics(Default::default(), "platform");
+        // println!("Output: {result:?}");
+        // println!("Retrieving topics for language: ");
+        // let result = output.retrieve_topics(Default::default(), "language");
+        // println!("Output: {result:?}");
+        // println!("Retrieving topics for database: ");
+        // let result = output.retrieve_topics(Default::default(), "database");
+        // println!("Output: {result:?}");
+
+        println!("Finished");
+        let projects = output.search(&Default::default());
+        println!("Projects is:\n{projects:#?}");
+        output
+    }
+
+    fn retrieve_topics(
+        &self,
+        parameters: &HashMap<String, String>,
+        target_topic: &str,
+    ) -> Vec<String> {
+        let topics = &self.topics;
+        let mut sections = vec![format!("{} IS NOT NULL", target_topic.to_string())];
+        for topic in topics {
+            if parameters.is_empty() {
+                // println!("Empty");
+                break;
+            }
+            // println!("topic: {topic}");
+            if parameters.contains_key(topic) {
+                if topic.eq(target_topic) {
+                    // println!("Target");
+                } else {
+                    // println!("Not Target");
+                    let value = parameters.get(topic).unwrap();
+                    let section = format!("{topic}={value}");
+                    sections.push(section);
+                };
+            }
+            // return topics.clone();
+        }
+        let sections = sections.join("\n    AND ");
+        // let inner_query = format!("    SELECT * FROM projects\n    WHERE {sections}");
+        // let full_query =
+        //     format!("SELECT DISTINCT {target_topic}\nFROM (\n{inner_query}\n) subquery");
+        let full_query = format!("SELECT DISTINCT {target_topic} FROM projects\n WHERE {sections}");
+        // println!("Running: {full_query}");
+        let mut output = Vec::with_capacity(topics.len());
+        for row in self
+            .connection
+            .prepare(full_query)
+            .unwrap()
+            .into_iter()
+            .map(|row| row.unwrap())
+        {
+            // Query will return single column rows, this probably isn't necessary
+            for (index, _topic) in topics.iter().enumerate() {
+                if row.contains(index) {
+                    let value: Result<&str, Error> = row.try_read::<&str, _>(index);
+                    // print!("{value} ");
+                    if let Ok(value) = value {
+                        // println!("{topic}: {}", value);
+                        output.push(value.to_string());
+                    }
+                }
+            }
+            // println!("-----------------------------");
+        }
+
+        output
+    }
+
+    fn search(&self, parameters: &HashMap<String, String>) -> Vec<Project> {
+        let query: &str = "SELECT * FROM projects;";
+        let mut projects: Vec<Project> = Vec::new();
+        for row in self
+            .connection
             .prepare(query)
             .unwrap()
             .into_iter()
             .map(|row| row.unwrap())
         {
-            for (index, topic) in topics.iter().enumerate() {
-                if row.contains(index) {
-                    let value: Result<&str, Error> = row.try_read::<&str, _>(index);
+            // println!("Setting source for row:\n{row:?}");
+            let source = row.try_read::<&str, _>(0).unwrap().to_string();
+            // println!("Source is {source}");
+
+            let topics = HashMap::new();
+            let mut project: Project = Project { source, topics };
+            // println!("Iterating through columns");
+            for (index, topic) in self.topics.iter().enumerate() {
+                if row.contains(index + 1) {
+                    // println!("Checking column {} for topic {topic}", index + 1);
+                    let value: Result<&str, Error> = row.try_read::<&str, usize>(index + 1);
                     if let Ok(value) = value {
-                        println!("{topic}: {}", value);
+                        // println!("Value is {value}");
+                        project.topics.insert(topic.to_string(), value.to_string());
                     }
+                } else {
+                    break;
                 }
             }
-            println!("\n-------------------------------------\n");
+            println!("Inserting Project: {project:#?}");
+            projects.push(project);
         }
-        println!("Finished");
-        Self { connection, topics }
+        projects
     }
-    // fn retrieve_topics(&self, parameters: HashMap<String, String>) -> Vec<Project> {
-    //     let topics = &self.topics;
-    //     for topic in topics {
-    //         if parameters.contains_key(topic) {
-    //             let value = parameters.get(topic).unwrap();
-
-    //         }
-    //     }
-    //     self.connection.execute(statement)
-    //     Vec::new()
-    // }
 }
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
 pub struct Project {
-    source: String,
+    pub source: String,
     #[serde(flatten)]
-    topics: HashMap<String, String>,
+    pub topics: HashMap<String, String>,
 }
